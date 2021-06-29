a9ca8c230b68ee4628475f8151408df9b2a6dd27
Bruno Moraes <bruno_bmoraes@hotmail.com>
2021-06-29 10:50:31 -0300

    config firebase

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
 void main() {
-  runApp(AppFirebase());
+  runApp(AppWidget());
 }
 
-class AppFirebase extends StatefulWidget {
-  @override
-  _AppFirebaseState createState() => _AppFirebaseState();
-}
-
-class _AppFirebaseState extends State<AppFirebase> {
+class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
