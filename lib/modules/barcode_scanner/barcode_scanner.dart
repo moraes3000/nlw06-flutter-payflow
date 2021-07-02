2d78182c55f87baed5f58e859df59f901f40cd9e
Bruno Moraes <bruno_bmoraes@hotmail.com>
2021-07-02 12:37:03 -0300

    virando a camera

--- lib/modules/barcode_scanner/barcode_scanner.dart
+++ lib/modules/barcode_scanner/barcode_scanner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
@@ -13,24 +14,39 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Escanie o código de barras do boleto",
-            style: TextStyles.buttonBackground),
-        leading: BackButton(
-          color: AppColors.background,
+    return RotatedBox(
+      quarterTurns: 1,
+      child: SafeArea(
+        top: true,
+        bottom: true,
+        left: true,
+        right: true,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Escanie o código de barras do boleto",
+                style: TextStyles.buttonBackground),
+            leading: BackButton(
+              color: AppColors.background,
+            ),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: Container(color: Colors.black.withOpacity(0.6)),
+              ),
+              Expanded(flex: 2, child: (Container(color: Colors.transparent))),
+              Expanded(child: Container(color: Colors.black.withOpacity(0.6)))
+            ],
+          ),
+          bottomNavigationBar: SetLabelButtons(
+            primaryLabel: 'Insira código no boleto',
+            primaryOnPressed: () {},
+            secondaryLabel: 'Add da galeria',
+            secondaryOnPressed: () {},
+          ),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(color: Colors.black),
-          ),
-          Expanded(flex: 2, child: (Container(color: Colors.transparent))),
-          Expanded(child: Container(color: Colors.black))
-        ],
-      ),
-      bottomNavigationBar: SetLabelButtons(),
     );
   }
 }
